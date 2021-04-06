c571c8f0a5714c0dfcf2677812cb74f0d7421b9f
肖连伟 <2695772510@qq.com>
2021-04-06 16:08:14 +0800

    1351

--- http.js
+++ http.js
@@ -1,16 +1,11 @@
 const http = require('http');
 
-const request = http.request('http://yuanjin.tech:5005/api/movie',{
-    method: "GET"
-},resp => {
-    console.log('服务器的状态码',resp.statusCode);
-    console.log('服务器响应头',resp.headers);
-    let content = '';
-    resp.on('data',data => {
-        content += data.toString('utf-8');
-    }) 
-    resp.on('end',() => {
-        console.log(JSON.parse(content));
-    })
-});
-request.end();
+const server = http.createServer((req,res) => {
+    console.log(req.url);
+    res.write('what the fuck!');
+    res.end();
+})
+
+server.listen(3000,() => {
+    console.log('listen server 3000');
+})
