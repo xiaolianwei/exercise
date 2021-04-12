c61badf62ea105cafa4d37ff23e51981023904fd
肖连伟 <2695772510@qq.com>
2021-04-12 08:41:32 +0800

    1351

--- http.js
+++ http.js
@@ -1,9 +1,13 @@
 const http = require('http');
+const url = require('url')
 
 const server = http.createServer((req,res) => {
-    console.log(req.url);
-    res.write('what the fuck!');
-    res.end();
+    console.log(url.parse(req.url,true));
+    res.writeHead(200,{
+        'Content-type': 'text/html;charset=utf-8',
+    })
+    res.write('<h1>费劲儿上课了</h1>')
+    res.end()
 })
 
 server.listen(3000,() => {
