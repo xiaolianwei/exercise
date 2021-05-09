2b2529410ef69ff8c5e46be43c679e41d3105679
肖连伟 <2695772510@qq.com>
2021-05-09 23:22:48 +0800

    sge

--- demo3.js
+++ demo3.js
@@ -1,4 +1,39 @@
+// const fs = require('fs');
+// const path = require('path');
 
-process.stdin.on('data',(data) => {
-    console.log(data.toString().trim());
+// const dirname = path.resolve(__dirname,'./myfile','abc.txt');
+
+// fs.readFile(dirname, function (err,data) {
+//     console.log(data.toString());
+// })
+
+function fun() {
+    // this = {name: 'foo'}
+    return () => {
+        return () => {
+            return () => {
+                console.log(this.name)
+            }
+        }
+    }
+}
+var f = fun.call({
+    name: 'foo'
+})
+
+// f = () => {
+//     return () => {
+//         return () => {
+//             console.log(this.name)
+//         }
+//     }
+// }
+
+var t1 = f.call()()()
+//
+var t2 = f().call({
+    name: 'baz'
+})()
+var t3 = f()().call({
+    name: 'qux'
 })
